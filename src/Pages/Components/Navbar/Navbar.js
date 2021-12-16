60812692c0c6baf57dcc708d9be515cdde29caa1
alexpinho <arp2323arp@gmail.com>
2021-12-16 22:42:54 +0000

    cleaning final touches + navbar start

--- src/Pages/Components/Navbar/Navbar.js
+++ src/Pages/Components/Navbar/Navbar.js
@@ -4,11 +4,12 @@ function Navbar (){
 
     return (
         <div className="navbar">
-                <ul className="navbar-menu">
-                    <li className="navbar-menu-elements">
+            <img src="img/nf.svg" className="navbar-logo" alt="" />
+            <ul className="navbar-menu">
+                <li className="navbar-menu-elements">
                     <a href='/info' className="navbar-text">info</a>
-                    </li>
-                </ul>
+                </li>
+            </ul>
         </div>
     );
 };
