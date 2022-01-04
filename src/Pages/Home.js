2e99bb28c5fa477c4711d872b585e77d47fdf2af
alexpinho <arp2323arp@gmail.com>
2022-01-04 13:46:03 +0000

    home projects full page scroll

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,12 +6,40 @@ function Home() {
   return (
     <Layout>
       <div className="home">
-        <CardProject
-          date="2020"
-          title="archive projects"
-          area="ACADEMIC"
-          team="Ana Catarina Ricardo Sacadura"
-        />
+        <div className="container">
+          <div className=" section section-one">
+            <CardProject
+              date="2020"
+              title="archive projects"
+              area="ACADEMIC"
+              team="Ana Catarina Ricardo Sacadura"
+            />
+          </div>
+          <div className=" section section-two">
+            <CardProject
+              date="2020"
+              title="queima das fitas 2021"
+              area="ACADEMIC"
+              team="Ana Catarina Ricardo Sacadura"
+            />
+          </div>
+          <div className=" section section-three">
+            <CardProject
+              date="2020"
+              title="alta e baixa"
+              area="ACADEMIC"
+              team="Ana Catarina Ricardo Sacadura"
+            />
+          </div>
+          <div className=" section section-four">
+            <CardProject
+              date="2020"
+              title="skillX"
+              area="ACADEMIC"
+              team="Ana Catarina Ricardo Sacadura"
+            />
+          </div>
+        </div>
       </div>
     </Layout>
   );
