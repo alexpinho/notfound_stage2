8307d5c10f256a7a5fe316d8653c85e6ff732fc2
alexpinho <arp2323arp@gmail.com>
2022-01-05 17:40:13 +0000

    text info responsive

--- src/Pages/Info.js
+++ src/Pages/Info.js
@@ -6,12 +6,12 @@ const Info = () => {
     <Layout>
       <div className="info">
         <div className="info-text text-big">
-          Hi, I'm Alexandre! <br /> I appreciate that you want to know a little
-          more about my work.
+          Hi, I'm Alexandre! I appreciate that you want to know a little more
+          about my work.
           <br />
           The portfolio presents my 4 best projects and also experiences, which
           reflect my skills and make you get to know me a little better.
-          <br />I hope you like it,
+          <br />I hope you like it, <span className="blink_me">l</span>
         </div>
       </div>
     </Layout>
