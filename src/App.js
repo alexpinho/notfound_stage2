d8f19e48c666a05f1c0aa0b9df1e386201ab339d
alexpinho <arp2323arp@gmail.com>
2022-01-06 14:31:34 +0000

    bug

--- src/App.js
+++ src/App.js
@@ -3,15 +3,51 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home";
 import Info from "./Pages/Info";
 import "./styles/applications.scss";
+import Loader from "./Pages/Components/Loader";
+import {
+  motion,
+  AnimatePresence,
+  AnimateSharedLayout,
+} from "framer-motion/dist/framer-motion";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loading
+      ? document.querySelector("body").classList.add("loading")
+      : document.querySelector("body").classList.remove("loading");
+  }, [loading]);
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/info" component={Info} exact />
-      </Switch>
-    </Router>
+    <AnimateSharedLayout type="crossfade">
+      <AnimatePresence>
+        {loading ? (
+          <motion.div key="loader">
+            <Loader setLoading={setLoading} />
+          </motion.div>
+        ) : (
+          <>
+            <Router>
+              <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/info" component={Info} exact />
+              </Switch>
+            </Router>
+            {!loading && (
+              <div className="transition-image final">
+                <motion.img
+                  transition={{ ease: [0.6, 0.01, -0.05, 0.9], duration: 1.6 }}
+                  src={process.env.PUBLIC_URL + `/images/image-2.jpg`}
+                  layoutId="main-image-1"
+                />
+              </div>
+            )}
+          </>
+        )}
+      </AnimatePresence>
+    </AnimateSharedLayout>
   );
 }
 
